7d23762fa9af5487080043e50eb023c1453d2316
joker2770 <1214220480@qq.com>
2022-12-01 10:23:56 +0800

    feat: add launcher scripts. :heavy_plus_sign:

---
+++ scripts/launcher.sh
@@ -0,0 +1,25 @@
+#########################################################################
+# File Name: launcher.sh
+# Author: Jintao Yang
+# mail: 18608842770@163.com
+# Created Time: 2022年01月14日 星期五 08时36分31秒
+#########################################################################
+
+#/***************************************************************************
+# *   Copyright (C) 2020-2022 Jintao Yang                                        *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+#!/bin/bash
+
+cd $SNAP/usr/local/share/games/synthetic-things
+$SNAP/usr/local/share/games/synthetic-things/synthetic-things
